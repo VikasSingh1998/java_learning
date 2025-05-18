5807fb1757d29094cc3bad2680eb67b58db3910a
Singh Vikas Kumar Arvind <98527226+VikasSingh1998@users.noreply.github.com>
2025-05-19 01:06:23 +0530

    Update ex1.java

--- 99_DurgaSirJavaExamples/A15_Generics/1_DurgaExamples/2_Advance/ex1.java
+++ 99_DurgaSirJavaExamples/A15_Generics/1_DurgaExamples/2_Advance/ex1.java
@@ -1,1 +1,12 @@
+📘 What is a Generic Class?  ==> Class with type-parameter.
+-----------------------------------------------------------------
+A Generic Class is a class that operates on a type parameter, allowing you to create a class 
+that can work with different data types without rewriting the code for each type.
 
+🔹 Syntax of Generic Class
+----------------------------
+class ClassName<T> {
+    // T can be used as a type inside the class
+}
+T is a type parameter, like a placeholder.
+It is replaced with a real type (e.g., Integer, String) when an object is created.
